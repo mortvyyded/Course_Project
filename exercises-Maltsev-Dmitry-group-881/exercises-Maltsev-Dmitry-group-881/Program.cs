52a048357001eaae7b1eb53cd0c5d78abe4211fe
mortvyyded <mr_dimama@mail.ru>
2021-03-10 00:57:35 +0300

    test
    test

--- exercises-Maltsev-Dmitry-group-881/exercises-Maltsev-Dmitry-group-881/Program.cs
+++ exercises-Maltsev-Dmitry-group-881/exercises-Maltsev-Dmitry-group-881/Program.cs
@@ -10,62 +10,69 @@ namespace exercises_Maltsev_Dmitry_group_881
     {
         static void Main(string[] args)
         {
-            int number = 1;
-
-            switch (number)
+            while (true)
             {
-                //Глава 1. Основы C#. Условные конструкции
-                case 1: //Упражнение 1
-                    Console.WriteLine("УПРАЖНЕНИЕ 1");
-                    Console.WriteLine("Введите первое число: ");
-                    int ex1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("Введите второе число: ");
-                    int ex1_1 = Convert.ToInt32(Console.ReadLine());
+                double x, y;
+                Console.WriteLine("\nВВЕДИТЕ НОМЕР УПРАЖНЕНИЯ (1-22):");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number >= 1 && number <= 22) {}
+                else
+                {
+                    Console.WriteLine("Неизвестное упражнение");
+                }
+                switch (number)
+                {
+                    //Глава 1. Основы C#. Условные конструкции
+                    case 1: //Упражнение 1
+                        Console.WriteLine("\nУПРАЖНЕНИЕ 1");
+                        Console.WriteLine("Введите первое число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите второе число: ");
+                        y = Convert.ToInt32(Console.ReadLine());
 
-                    if (ex1 > ex1_1)
-                    {
-                        Console.WriteLine("Первое число больше второго");
-                    }
-                    else if (ex1 < ex1_1)
-                    {
-                        Console.WriteLine("Первое число меньше второго");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Оба числа равны");
-                    }
-                    goto case 2;
-                case 2: //Упражнение 2
-                    Console.WriteLine("УПРАЖНЕНИЕ 2");
-                    Console.WriteLine("Введите число: ");
-                    int ex2 = Convert.ToInt32(Console.ReadLine());
+                        if (x > y)
+                        {
+                            Console.WriteLine("Первое число больше второго");
+                        }
+                        else if (x < y)
+                        {
+                            Console.WriteLine("Первое число меньше второго");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Оба числа равны");
+                        }
+                        break;
+                    case 2: //Упражнение 2
+                        Console.WriteLine("\nУПРАЖНЕНИЕ 2");
+                        Console.WriteLine("Введите число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
 
-                    if (ex2 > 5 && ex2 < 10)
-                    {
-                        Console.WriteLine("Число больше 5 и меньше 10");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Неизвестное число");
-                    }
-                    goto case 3;
-                case 3: //Упражнение 3
-                    Console.WriteLine("УПРАЖНЕНИЕ 3");
-                    Console.WriteLine("Введите число: ");
-                    int ex3 = Convert.ToInt32(Console.ReadLine());
+                        if (x > 5 && x < 10)
+                        {
+                            Console.WriteLine("Число больше 5 и меньше 10");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неизвестное число");
+                        }
+                        break;
+                    case 3: //Упражнение 3
+                        Console.WriteLine("\nУПРАЖНЕНИЕ 3");
+                        Console.WriteLine("Введите число: ");
+                        x = Convert.ToInt32(Console.ReadLine());
 
-                    if (ex3 == 5 || ex3 == 10)
-                    {
-                        Console.WriteLine("Число либо равно 5, либо равно 10");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Неизвестное число");
-                    }
-
-                    break;
+                        if (x == 5 || x == 10)
+                        {
+                            Console.WriteLine("Число либо равно 5, либо равно 10");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неизвестное число");
+                        }
+                        break;
+                }
             }
-            Console.ReadKey();
         }
     }
 }

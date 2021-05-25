9be2eca850fe0a35a9aa2922c1d5653b63169f4e
mortvyyded <mr_dimama@mail.ru>
2021-05-25 22:14:31 +0300

    test
    test

---
+++ YAzyki programmirovaniya/SharpGL_My_First_Model/SharpGL_My_First_Model/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace SharpGL_My_First_Model
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openglControl1 = new SharpGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.openglControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openglControl1
+            // 
+            this.openglControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.openglControl1.DrawFPS = false;
+            this.openglControl1.Location = new System.Drawing.Point(13, 12);
+            this.openglControl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.openglControl1.Name = "openglControl1";
+            this.openglControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openglControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openglControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openglControl1.Size = new System.Drawing.Size(774, 426);
+            this.openglControl1.TabIndex = 0;
+            this.openglControl1.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
+            this.openglControl1.Resized += new System.EventHandler(this.openGLControl_Resized);
+            this.openglControl1.Load += new System.EventHandler(this.openglControl1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.openglControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.openglControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openglControl1;
+    }
+}
+
---
+++ YAzyki programmirovaniya/SharpGL_My_First_Model/SharpGL_My_First_Model/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace SharpGL_My_First_Model
+{
+    public partial class Form1 : Form
+    {
+        float rotation = 0.0f; // переменная, которая будет задавать угол поворота
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openglControl1_Load(object sender, EventArgs e)
+        {
+            //  Возьмём OpenGL объект
+            OpenGL gl = openglControl1.OpenGL;
+
+            //  Очищаем буфер цвета и глубины
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //  Загружаем единичную матрицу
+            gl.LoadIdentity();
+
+            //  Указываем оси вращения (x, y, z)
+            gl.Rotate(rotation, 0.0f, 1.0f, 0.0f);
+
+            // рисуем крышу
+            gl.Begin(OpenGL.GL_TRIANGLES);
+
+            gl.Color(1f, 0.2f, 0.0f); // здесь задаём цвет для каждой плоскости
+            gl.Vertex(0.0f, 2.5f, 0.0f);
+            gl.Vertex(2.0f, 1.5f, -2.0f);
+            gl.Vertex(2.0f, 1.5f, 2.0f);
+
+            gl.Color(1f, 0.3f, 0.0f);
+            gl.Vertex(-2.0f, 1.5f, -2.0f);
+            gl.Vertex(-2.0f, 1.5f, 2.0f);
+            gl.Vertex(0.0f, 2.5f, 0.0f);
+
+            gl.Color(1f, 0.4f, 0.0f);
+            gl.Vertex(0.0f, 2.5f, 0.0f);
+            gl.Vertex(2.0f, 1.5f, -2.0f);
+            gl.Vertex(-2.0f, 1.5f, -2.0f);
+
+            gl.Color(1f, 0.1f, 0.0f);
+            gl.Vertex(0.0f, 2.5f, 0.0f);
+            gl.Vertex(-2.0f, 1.5f, 2.0f);
+            gl.Vertex(2.0f, 1.5f, 2.0f);
+
+            gl.End();
+
+            // передняя часть дома
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Color(1f, 1f, 0f);
+            gl.Vertex(2f, 1.5f, -2f);
+            gl.Vertex(2f, 0f, -2f);
+            gl.Vertex(-2f, 0f, -2f);
+            gl.Vertex(-2f, 1.5f, -2f);
+
+            gl.End();
+
+            // правая часть дома
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Color(1f, 0.8f, 0f);
+            gl.Vertex(2f, 0f, -2f);
+            gl.Vertex(2f, 0f, 2f);
+            gl.Vertex(2f, 1.5f, 2f);
+            gl.Vertex(2f, 1.5f, -2f);
+            gl.End();
+
+            // задняя часть дома
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Color(1f, 0.7f, 0f);
+            gl.Vertex(2f, 0f, 2f);
+            gl.Vertex(-2f, 0f, 2f);
+            gl.Vertex(-2f, 1.5f, 2f);
+            gl.Vertex(2f, 1.5f, 2f);
+            gl.End();
+
+            // левая часть дома
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Color(1f, 0.9f, 0f);
+            gl.Vertex(-2f, 0f, -2f);
+            gl.Vertex(-2f, 0f, 2f);
+            gl.Vertex(-2f, 1.5f, 2f);
+            gl.Vertex(-2f, 1.5f, -2f);
+            gl.End();
+
+            // дверь (передняя стена)
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Color(1f, 0.5f, 0f);
+            gl.Vertex(0.3f, 0f, -2.01f);
+            gl.Vertex(-0.3f, 0f, -2.01f);
+            gl.Vertex(-0.3f, 1.2f, -2.01f);
+            gl.Vertex(0.3f, 1.2f, -2.01f);
+            gl.End();
+
+            // рисуем землю            
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Color(0f, 1f, 0f);
+            gl.Vertex(-10f, 0f, -10f);
+            gl.Vertex(10f, 0f, -10f);
+            gl.Vertex(10f, 0f, 10f);
+            gl.Vertex(-10f, 0f, 10f);
+            gl.End();
+
+            rotation += 1.5f; // угол разворота за 1 кадр
+        }
+
+        // Эту функцию используем для задания некоторых значений по умолчанию
+        private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+        {
+            //  Возьмём OpenGL объект
+            OpenGL gl = openglControl1.OpenGL;
+
+            //  Фоновый цвет по умолчанию (в данном случае цвет голубой)
+            gl.ClearColor(0.1f, 0.5f, 1.0f, 0);
+        }
+
+        // Данная функция используется для преобразования изображения 
+        // в объёмный вид с перспективой
+        private void openGLControl_Resized(object sender, EventArgs e)
+        {
+            //  Возьмём OpenGL объект
+            OpenGL gl = openglControl1.OpenGL;
+
+            //  Зададим матрицу проекции
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+            //  Единичная матрица для последующих преобразований
+            gl.LoadIdentity();
+
+            //  Преобразование
+            gl.Perspective(60.0f, (double)Width / (double)Height, 0.01, 100.0);
+
+            //  Данная функция позволяет установить камеру и её положение
+            gl.LookAt(-5, 3, -6,    // Позиция самой камеры (x, y, z)
+                        0, 1, 0,     // Направление, куда мы смотрим
+                        0, 2, 0);    // Верх камеры
+
+            //  Зададим модель отображения
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+    }
+}

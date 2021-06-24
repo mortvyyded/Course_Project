ad79b7d0b86d1876b16622170be4831e05468238
mortvyyded <mr_dimama@mail.ru>
2021-06-25 00:51:20 +0300

    test
    test

--- YAzyki programmirovaniya/5 zadach' po fizike na 7 yazykakh programmirovaniya/Задачи Python/Задача1.py
+++ YAzyki programmirovaniya/5 zadach' po fizike na 7 yazykakh programmirovaniya/Задачи Python/Задача1.py
@@ -6,36 +6,36 @@ from tkinter import ttk
 window = tk.Tk()
 
 window.geometry('600x400')
-lbl1 = Label(window, fg='#f00', text="A=", font=("Arial Bold", 24))
+lbl1 = Label(window, fg='#f00', text="Ответ v =", font=("Arial Bold", 24))
 lbl1.grid(column=0, row=0) 
-lbl2 = Label(window, fg='#f00', text="P=", font=("Arial Bold", 24))
+lbl2 = Label(window, fg='#f00', text="S =", font=("Arial Bold", 24))
 lbl2.grid(column=0, row=1)  
-lbl3 = Label(window, fg='#f00', text="h=", font=("Arial Bold", 24))
+lbl3 = Label(window, fg='#f00', text="t =", font=("Arial Bold", 24))
 lbl3.grid(column=0, row=2)  
  
 
-A= tk.IntVar()
-nameEntered1 = ttk.Entry(window, width = 30, foreground='#f00', textvariable = A)
+v = tk.IntVar()
+nameEntered1 = ttk.Entry(window, width = 30, foreground='#f00', textvariable = v)
 nameEntered1.grid(column = 1, row = 0)
-P = tk.IntVar()
-nameEntered2 = ttk.Entry(window, width = 30, foreground='#f00', textvariable = P)
+S = tk.IntVar()
+nameEntered2 = ttk.Entry(window, width = 30, foreground='#f00', textvariable = S)
 nameEntered2.grid(column = 1, row = 1)
-h = tk.IntVar()
-nameEntered3 = ttk.Entry(window, width = 30, foreground='#f00', textvariable = h)
+t = tk.IntVar()
+nameEntered3 = ttk.Entry(window, width = 30, foreground='#f00', textvariable = t)
 nameEntered3.grid(column = 1, row = 2)
 
 
 def Rabotai():
-	d = P.get()*(h.get())
+	v = S.get()/(t.get())
 	
 	nameEntered1.delete(0,"")
 	nameEntered1.insert(0,d) 
 
-b1 = Button(text="Вычислить", width=30, height=3, foreground='#f00', command=Rabotai)
+b1 = Button(text="Решить", width=30, height=3, foreground='#f00', command=Rabotai)
 b1.grid(column=1, row=5)
 
 canvas = tk.Canvas(window, height=100, width=400)
-image = Image.open("Снимок экрана 2021-06-19 222349.png")
+image = Image.open("zadacha1.png")
 photo = ImageTk.PhotoImage(image)
 image = canvas.create_image(0,0, anchor='nw',image=photo)
 canvas.grid(row=6,column=1)
